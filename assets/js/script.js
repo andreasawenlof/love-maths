dfd1df20e59dde038da645e8a88457a2f3cb6c90
mezter1337 <andreas.javert@gmail.com>
2024-03-18 06:22:00 +0100

    Add favicon, and final tidy ups for the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,13 @@ document.addEventListener('DOMContentLoaded', function () {
         })
     }
 
+    document.getElementById('answer-box').addEventListener('keydown', function(event) 
+    {
+        if(event.key === 'Enter') {
+            checkAnswer();
+        }
+    })
+
     runGame('addition');
 });
 
@@ -24,11 +31,13 @@ document.addEventListener('DOMContentLoaded', function () {
  */
 
 function runGame(gameType) {
+    // Clear answer-box after submit
+    document.getElementById('answer-box').value='';
+    document.getElementById('answer-box').focus();
+    
     // Creates two random numbers between 1 and 25
     const num1 = Math.floor(Math.random() * 25) + 1;
     const num2 = Math.floor(Math.random() * 25) + 1;
-
-    let sum = 0
 
     if (gameType === 'addition') {
         displayAdditionQuestion(num1, num2);
